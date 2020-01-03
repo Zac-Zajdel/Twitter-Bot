ba73862e74b2cda1bd614ed9eba2c478aa11f355
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2020-01-03 01:47:53 -0500

    Small changes to bot.js file

--- bot.js
+++ bot.js
@@ -7,27 +7,17 @@ const myTweetObj = {
   twitter_handle: "ITSONLYMYTHIRD3"
 };
 
-// My EC2 instance is always running so this will make it specifically run once on the third day of every month at 3:00pm.
-let now = new Date();
-if (
-  now.getDate() == 3 &&
-  now.getHours() == 15 &&
-  now.getMinutes() == 0 &&
-  now.getSeconds() == 0
-) {
-  tweetDankMeme(myTweetObj);
-}
+tweetDankMeme(myTweetObj);
 
 function tweetDankMeme(myTweetObj) {
-  const T = new Twit(config);
+  const TWEET = new Twit(config);
+  const PATH = path.join(__dirname, "./video.mp4");
 
-  const PATH = path.join(__dirname, "./videoplayback.mp4");
-
-  T.postMediaChunked({ file_path: PATH }, function(err, data, response) {
+  TWEET.postMediaChunked({ file_path: PATH }, function(err, data, response) {
     const mediaIdStr = data.media_id_string;
     const meta_params = { media_id: mediaIdStr };
 
-    T.post("media/metadata/create", meta_params, function(err, data, response) {
+    TWEET.post("media/metadata/create", meta_params, function(err, data, response) {
       if (!err) {
         const params = { status: myTweetObj.content, media_ids: [mediaIdStr] };
 
